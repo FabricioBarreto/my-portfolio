297b918144d42ffc71d836f1dd4b81a10c51f0ff
Fabricio Barreto <fabrib40@gmail.com>
2024-12-02 19:16:38 -0300

    first commit

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -40,12 +40,14 @@ const EmailSection = () => {
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-red-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <div>
-          <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+          <h5 className="text-xl font-bold text-white my-2">
+            Let&apos;s Connect
+          </h5>
           <p className="text-[#ADB7BE] mb-4 max-w-md">
             {" "}
-            I'm currently looking for a new opportunity, my imbox is alway open.
-            Whether you have a question or just want to say hi, I'll try my best
-            to get back to you!
+            I&apos;m currently looking for a new opportunity, my imbox is alway
+            open. Whether you have a question or just want to say hi, I&apos;ll
+            try my best to get back to you!
           </p>
           <div className="socials flex flex-row gap-2">
             <Link href="https://github.com/FabricioBarreto">
@@ -121,5 +123,4 @@ const EmailSection = () => {
     </section>
   );
 };
-
 export default EmailSection;
